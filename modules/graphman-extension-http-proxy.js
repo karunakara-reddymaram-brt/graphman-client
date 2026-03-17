ca8654620d59e44981444740dc63e5357b127b9d
karunakar <karunakara-reddy.maram@broadcom.com>
2026-03-17 15:02:37 +0530

    updating configuration of proxu address to host and port (#149)
    * updating configuration of proxu address to host and port
    
    * addressing review comments
    
    * addressing review comments
    
    * addressing review comments
    
    * addressing review comments
    
    * addressing review comments

--- modules/graphman-extension-http-proxy.js
+++ modules/graphman-extension-http-proxy.js
@@ -11,9 +11,9 @@ module.exports = {
      */
     apply: function (input, context) {
 
-        if(input.agentType === "socks") {
+        if(input.agentType?.startsWith("socks")) {
            return createSocksProxyAgent(input, context);
-       } else {
+        } else {
            return createHttpProxyAgent(input, context);
        }
     }
@@ -25,7 +25,7 @@ function createSocksProxyAgent(input, context) {
 
     try {
         const { SocksProxyAgent } = require("socks-proxy-agent");
-        agent = new SocksProxyAgent(proxyConfig.url || new URL(input.address), proxyConfig);
+        agent = new SocksProxyAgent(proxyConfig.url , proxyConfig);
     } catch (e) {
       throw "failed to configure socks proxy agent" + e.message;
     }
@@ -36,19 +36,6 @@ function createHttpProxyAgent(input, context) {
     let agent = null;
     const isHttps = context.gateway["address"].startsWith('https://');
     const proxyConfig = createProxyConfig(input);
-    const proxyUrlLower = input.address.toLowerCase();
-    const isProxyHttps = proxyUrlLower.startsWith('https://');
-
-    if (isProxyHttps) {
-        // If proxy URL uses https://, ensure TLS options are configured if needed
-        if (!proxyConfig.tls) {
-            proxyConfig.tls = {};
-        }
-        // If rejectUnauthorized is not explicitly set for proxy TLS, default to false for compatibility
-        if (proxyConfig.tls.rejectUnauthorized === undefined) {
-            proxyConfig.tls.rejectUnauthorized = false;
-        }
-    }
 
     try {
         // Use https-proxy-agent for HTTPS targets, http-proxy-agent for HTTP targets
@@ -56,10 +43,10 @@ function createHttpProxyAgent(input, context) {
 
         if (isHttps) {
             const { HttpsProxyAgent } = require("https-proxy-agent")
-            agent = new HttpsProxyAgent(input.address, proxyConfig);
+            agent = new HttpsProxyAgent(proxyConfig.url, proxyConfig);
         } else {
             const { HttpProxyAgent } = require("http-proxy-agent")
-            agent = new HttpProxyAgent(input.address, proxyConfig);
+            agent = new HttpProxyAgent(proxyConfig.url, proxyConfig);
         }
 
     } catch (e) {
@@ -72,31 +59,17 @@ function createHttpProxyAgent(input, context) {
 function createProxyConfig(obj) {
     const proxy = {};
     const cred = obj.credentialRef;
-    let auth;
+    const url = new URL(`${obj.agentType}://${obj.host}:${obj.port}`);
 
     if (cred) {
-        if (obj.agentType === "socks") {
-            const url = new URL(obj.address);
-            url.username = cred.username;
-            url.password = cred.password;
-
-            proxy.url = url;
-        } else {
-            auth = `Basic ${Buffer.from(`${cred.username}:${cred.password}`).toString('base64')}`;
-        }
+        url.username = cred.username;
+        url.password = cred.password;
     }
 
     Object.keys(obj.options).forEach(key => {
         proxy[key] = obj.options[key];
     });
 
-    if (!proxy.headers) {
-        proxy.headers = {};
-    }
-
-    if (auth) {
-        proxy.headers["Proxy-Authorization"] = auth;
-    }
-
+    proxy.url = url;
     return proxy;
 }
